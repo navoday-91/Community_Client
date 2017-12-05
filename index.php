771202a49cac201e3e210b32cd2686d032f028e1
navoday-91 <navoday.91@gmail.com>
2017-12-05 14:42:46 -0800

    CMPE281

--- index.php
+++ index.php
@@ -32,6 +32,9 @@
           </div>
           <?php
             session_start();
+            $_SESSION['community'] = $_GET['community'];
+            $community = $_GET['community'];
+            $_SESSION['error'] = $community.$_SESSION['community'];
           ?>
           <?php if (isset($_SESSION['error'])){ ?>
           <div class="text-center pad-top-20">
